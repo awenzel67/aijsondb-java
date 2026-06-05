5a037ae7b42b2a9e39f681539f27b3a428bd1f95
Andreas Wenzel <awenzel67@gmail.com>
2026-06-05 16:02:55 +0200

    langchain4j sample with OpenAI and Ollama

--- scripts/aijsondbjlangchain.java
+++ scripts/aijsondbjlangchain.java
@@ -2,12 +2,16 @@
 //JAVA 25+
 //DEPS io.github.awenzel67:aijsondb-java:0.0.3
 //DEPS dev.langchain4j:langchain4j-mistral-ai:1.14.1
+//DEPS dev.langchain4j:langchain4j-open-ai:1.14.1
+//DEPS dev.langchain4j:langchain4j-ollama:1.14.1
 //DEPS dev.langchain4j:langchain4j:1.14.1
 //DEPS io.github.awenzel67:aijsondb-agent:0.0.3
 
 import io.github.awenzel67.AIJsonDBC;
 import io.github.awenzel67.Agent;
 import dev.langchain4j.model.mistralai.MistralAiChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.chat.ChatModel;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,24 +26,55 @@ void main(String... args) {
         //Agent aiJsonAgent = Agent.create("../data/500 KB_V2.json","../data/employeeSchemaDescription_V2.json",Agent.E_PROMPT_TEMPLATE.GENERIC);
         String FILE_URL="https://www.berlin.de/sen/bildung/service/daten/kitaliste-nov-2025.xlsx";
         String FILE_NAME="kitaliste-nov-2025.xlsx";
-        InputStream in = new URI(FILE_URL).toURL().openStream();
-        Files.copy(in, Paths.get(FILE_NAME), StandardCopyOption.REPLACE_EXISTING);
+        //InputStream in = new URI(FILE_URL).toURL().openStream();
+        //Files.copy(in, Paths.get(FILE_NAME), StandardCopyOption.REPLACE_EXISTING);
 
         Agent aiJsonAgent = new Agent();
         aiJsonAgent.importData(FILE_NAME,"excel_temp_kita.json","excel_temp_kita_schema.json",Agent.E_PROMPT_TEMPLATE.GENERIC);
         System.out.println("Data loaded successfully.");
-        String apiKey = System.getenv("MISTRAL_API_KEY");
-        if (apiKey == null || apiKey.isEmpty()) {
-            System.err.println("Error: Please set MISTRAL_API_KEY environment variable.");
-            return;
-        }
+        String apiKey = null;
+        String modelType="Ollama";
+        ChatModel model = null;
+       System.out.println("Using model: " + modelType);
 
-        ChatModel model = MistralAiChatModel.builder()
+        if (modelType.equals("mistral")) {
+            apiKey = System.getenv("MISTRAL_API_KEY");
+            if (apiKey == null || apiKey.isEmpty()) {
+                System.err.println("Error: Please set MISTRAL_API_KEY environment variable.");
+                return;
+            }
+             model= MistralAiChatModel.builder()
                 .apiKey(apiKey)
                 .modelName("mistral-large-latest") // or "mistral-small", "mistral-large"
                 .temperature(0.0) // deterministic output
                 .maxTokens(2000)
                 .build();
+        
+        } 
+        else if (modelType.equals("OpenAI")) {
+            // Example for GPT4All local model
+           model = OpenAiChatModel.builder()
+            .apiKey(System.getenv("OPENAI_API_KEY"))
+            .modelName("gpt-5.4-mini")
+            .build();
+        }
+        else if (modelType.equals("Ollama")) {
+            // Example for GPT4All local model
+            String ollamaUrl = "http://192.168.0.124:11434";
+            model = OllamaChatModel.builder()
+            .baseUrl(ollamaUrl)
+            .temperature(0.0)
+            .logRequests(true)
+            .logResponses(true)
+            .numCtx(2000)
+            .think(false) // enables the THINK tool which allows the model to decide when to call tools and when to think
+            .modelName("qwen3.6:35b-a3b")
+            .build();
+        }
+        else {
+            System.err.println("Unsupported model type: " + modelType);
+            return;
+        }
 
         Agent.AIJsonAgent agent = AiServices.builder(Agent.AIJsonAgent.class)
                 .chatModel(model)
@@ -48,54 +83,37 @@ void main(String... args) {
                 .systemMessage(aiJsonAgent.getAgentSystemPrompt())
                 .build();
         
-        // STEP 3: User gets the final response from the agent
-        /* 
-        String userMessage = "How many employees are in the company?";
-        String answer = agent.chat(userMessage);
-        System.out.println(answer);
-        userMessage = "Which employees have the greatest experience?";
-        answer = agent.chat(userMessage);
-        System.out.println(answer);
-        */
+    
        {
-        String userMessage = "Which daycare center from the table offers the most places?";
-         System.out.println("User message: " + userMessage);
-        String answer = agent.chat(userMessage);
-        System.out.println("Answer: " + answer);
-       }
-
-       /*
-       {
-        String userMessage = "Wieviel Kitas sind in der Tabelle mit Kitas aufgeführt?";
+        String userMessage = "How many daycares are listed in the table of daycares?"; //"Wieviel Kitas sind in der Tabelle mit Kitas aufgeführt?";
          System.out.println("User message: " + userMessage);
         String answer = agent.chat(userMessage);
         System.out.println("Answer: " + answer);
        }
        {
-        String userMessage = "Welche Kita aus der Tabelle stellt die meisten Plätze zur Verfügung?";
+        String userMessage = "Which daycare center from the table offers the most places?"; //"Welche Kita aus der Tabelle stellt die meisten Plätze zur Verfügung?";
          System.out.println("User message: " + userMessage);
         String answer = agent.chat(userMessage);
         System.out.println("Answer: " + answer);
        }
        {
-        String userMessage = "Wieviele Kitas gibt es in Pankow?";
+        String userMessage = "How many daycare centers are there in Pankow?"; //"Wieviele Kitas gibt es in Pankow?";
          System.out.println("User message: " + userMessage);
         String answer = agent.chat(userMessage);
         System.out.println("Answer: " + answer);
        }
        {
-        String userMessage = "Wieviele Kitaplätze gibt es in Spandau?";
+        String userMessage = "How many daycare places are there in Spandau?";//"Wieviele Kitaplätze gibt es in Spandau?";
          System.out.println("User message: " + userMessage);
         String answer = agent.chat(userMessage);
         System.out.println("Answer: " + answer);
        }
-    {
-        String userMessage = "Welcher Stadtbezirk aus der Tabelle hat die meisten Kitaplätze?";
+      {
+        String userMessage = "Which district in the table has the most daycare places?";//"Welcher Stadtbezirk aus der Tabelle hat die meisten Kitaplätze?";
          System.out.println("User message: " + userMessage);
         String answer = agent.chat(userMessage);
         System.out.println("Answer: " + answer);
        }
-    */
     } 
     catch (Exception e) {
         System.err.println("Error loading data: " + e.getMessage());
